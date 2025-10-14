4e8590220f4bb39be49aa177f894cd936543d8b9
_iamcarlitos2 <crosadoro16@gmail.com>
2025-10-14 18:59:57 +0200

    Se modifica ejercicio 17 y se añade nuevamente el 16

--- src/ejercicio16.java
+++ src/ejercicio16.java
@@ -2,33 +2,24 @@ import java.util.Scanner;
 public class ejercicio16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //Pedimos al usuario que ingrese las horas
-        System.out.println("Ingresa las horas (0-23)");
-        int horas = sc.nextInt();
-        //Pedimos que introduzca los minutos
-        System.out.println("Introduce los minutos (0-59)");
-        int minutos = sc.nextInt();
-        //Pedinmos al ususario que ingrese los segundos
-        System.out.println("Ingrese los segundos (0-59)");
-        int segundos = sc.nextInt();
 
-        //sumamos los segundos el ++ es lo mismo que poner segundos = segundos + 1;
-        segundos++;
+        //Vamos a pedir que introduzca una calificacion del 0 al 10
+        System.out.println("Introduce una califiacion entre el 0 y el 10");
+        int calificacion = sc.nextInt();
 
-        //Hacemos las condicionales
-        if (segundos == 60){
-            segundos = 0;
-            minutos++;
-        }
-        if (minutos == 60) {
-            minutos = 0;
-            horas++;
-        }
-        if (horas == 24) {
-            horas = 0;
-        }
-
-        //Mostramos por consola el resultado
-        System.out.printf("La hora será: ", horas, minutos, segundos); //Colocamos printf, ya que nos permite colocar dos digitos
+        //Realizamos las condicionales para ver la calificacion de la nota
+         if (calificacion <0 || calificacion>10){
+             System.out.println("Error: La calificacion debe de estar entre 0 y 10");
+         } else if (calificacion < 3) {
+             System.out.println("Muy deficiente");
+         } else if (calificacion < 5) {
+             System.out.println("Deficiente");
+         } else if (calificacion < 6) {
+             System.out.println("Bien");
+         } else if (calificacion < 9){
+             System.out.println("Notable");
+         } else if (calificacion >= 9) {
+             System.out.println("Sobresaliente");
+         }
     }
 }
---
+++ src/ejercicio17.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class ejercicio17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Pedimos al usuario que ingrese las horas
+        System.out.println("Ingresa las horas (0-23)");
+        int horas = sc.nextInt();
+        //Pedimos que introduzca los minutos
+        System.out.println("Introduce los minutos (0-59)");
+        int minutos = sc.nextInt();
+        //Pedinmos al ususario que ingrese los segundos
+        System.out.println("Ingrese los segundos (0-59)");
+        int segundos = sc.nextInt();
+
+        //sumamos los segundos el ++ es lo mismo que poner segundos = segundos + 1;
+        segundos++;
+
+        //Hacemos las condicionales
+        if (segundos == 60){
+            segundos = 0;
+            minutos++;
+        }
+        if (minutos == 60) {
+            minutos = 0;
+            horas++;
+        }
+        if (horas == 24) {
+            horas = 0;
+        }
+
+        //Mostramos por consola el resultado
+        System.out.printf("La hora será: ", horas, minutos, segundos); //Colocamos printf, ya que nos permite colocar dos digitos
+    }
+}
+
