5a70ad07e1cf1b39618c45eaeb6005abe30ca21e
_iamcarlitos2 <crosadoro16@gmail.com>
2025-10-22 16:34:20 +0200

    Se modifica la variable del volumen de la esfera

--- src/unidad4_1/ejercicio5.java
+++ src/unidad4_1/ejercicio5.java
@@ -11,9 +11,9 @@ public class ejercicio5 {
         //calculamos la longitud de la circunferencia
         double longitud_circunferencia =  (2 * Math.PI * radio); //usamos double ya que java no puede pasar de un entero a un decimal
         double area_circulo = (Math.PI * radio * radio);
-        double volumen_esfera = ((3.0/4.0)* Math.PI * radio * radio);
+        double volumen_esfera = ((4.0/3.0)* Math.PI * radio * radio);
         //Mostramos el resultado por consola
-        System.out.println("La longitu de la circunferencia es de:" + longitud_circunferencia);
+        System.out.println("La longitud de la circunferencia es de:" + longitud_circunferencia);
         System.out.println("El area del circulo es de:" + area_circulo);
         System.out.println("El volumen de la esfera es de:" + volumen_esfera);
 
