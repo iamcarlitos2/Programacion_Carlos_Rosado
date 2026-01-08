a7dc5f21b4f764018fb2a0420e514d350597b0a2
_iamcarlitos2 <crosadoro16@gmail.com>
2026-01-08 18:55:23 +0100

    .

---
+++ src/unidad_6_arrays/ejercicios_C/ejercicio_2.java
@@ -0,0 +1,6 @@
+//Crea un programa que cree una matriz de 10x10 e introduzca los valores de las tablas de
+//multiplicar del 1 al 10 (cada tabla en una fila). Luego mostrará la matriz por pantalla
+package unidad_6_arrays.ejercicios_C;
+
+public class ejercicio_2 {
+}
